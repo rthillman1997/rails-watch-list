41629a14575cb4513340ccd2f36f4f7ef1193b86
Ryan Hillman <rthillman1997@gmail.com>
2022-11-18 16:38:47 +0000

    watch list

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :lists do
+    resources :bookmarks
+  end
+  resources :bookmarks, only: [:destroy]
+end
   # Defines the root path route ("/")
   # root "articles#index"
-end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Movie.create(title: "Wonder Woman 1984", overview: "Wonder Woman comes into conflict with the Soviet Union during the Cold War in the 1980s", poster_url: "https://image.tmdb.org/t/p/original/8UlWHLMpgZm9bx6QYh0NFoq67TZ.jpg", rating: 6.9)
+Movie.create(title: "The Shawshank Redemption", overview: "Framed in the 1940s for double murder, upstanding banker Andy Dufresne begins a new life at the Shawshank prison", poster_url: "https://image.tmdb.org/t/p/original/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg", rating: 8.7)
+Movie.create(title: "Titanic", overview: "101-year-old Rose DeWitt Bukater tells the story of her life aboard the Titanic.", poster_url: "https://image.tmdb.org/t/p/original/9xjZS2rlVxm8SFx8kPC3aIGCOYQ.jpg", rating: 7.9)
+Movie.create(title: "Ocean's Eight", overview: "Debbie Ocean, a criminal mastermind, gathers a crew of female thieves to pull off the heist of the century.", poster_url: "https://image.tmdb.org/t/p/original/MvYpKlpFukTivnlBhizGbkAe3v.jpg", rating: 7.0)
+Movie.create(title: "Harry Potter", overview: "You're a wizard Harry", poster_url: "https://c8.alamy.com/comp/PM5W1F/poster-daniel-radcliffe-as-harry-potter-in-warner-bros-pictures-fantasy-adventure-harry-potter-and-the-deathly-hallows-part-2-a-warner-bros-pictures-release-PM5W1F.jpg", rating: 9.0)
+Movie.create(title: "Kill Bill", overview: "Bad ass lady tries to kill old lover", poster_url: "https://cdn.shopify.com/s/files/1/0057/3728/3618/products/KILLBILL.VOL.1.PW_500x.jpg?v=1574966219", rating: 8.0)
